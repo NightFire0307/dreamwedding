cccc6810c93cf6ed3f21641dfd4bb113ba173661
DongyiWen <1035612457@qq.com>
2019-03-30 11:15:07 +0800

    Add Export Excel

--- dreamwedding/yeji/admin.py
+++ dreamwedding/yeji/admin.py
@@ -2,10 +2,13 @@
 import os
 from django.contrib import admin
 from django.contrib.admin import helpers
+from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.urls import path
 from django.shortcuts import render
 from daterange_filter.filter import DateRangeFilter
+from openpyxl import Workbook
+from io import BytesIO
 
 from .models import Shejishi, Yeji
 from .views import download_yejiexcel
@@ -36,6 +39,7 @@ class YejiAdmin(admin.ModelAdmin):
     # list_editable = ['colored_chujian']
 
     save_on_top = True
+    actions = ['export_excel']
     actions_on_top = True
     actions_on_bottom = True
     search_fields = ['order', 'jiaogao_own__name']
@@ -76,3 +80,40 @@ class YejiAdmin(admin.ModelAdmin):
                                                  list([(None, {'fields': form.base_fields})]),
                                                  {})
         return render(request, 'admin/yeji/extras/upload_excel.html', context)
+
+    def export_excel(self, request, queryset):
+        response = HttpResponse(content_type='applicationi/ms-excel')
+        response['Content-Disposition'] = 'attachment;filename=OutPut.xlsx'
+
+        wb = Workbook()
+        ws = wb.active
+
+        header = ['订单号', '客户姓名', '校稿时间',
+                  '套系金额', '加选金额', '设计人员',
+                  '出件时间', '备注', '张数', '订单所属部门']
+
+        for column in range(1, len(header) + 1):
+            ws.cell(row=1, column=column, value=header[column - 1])
+
+        print(queryset)
+        data_row = queryset.count() + 2
+        for row in range(2, data_row):
+            for column, data in enumerate(queryset, start=1):
+                ws.cell(row=row, column=column, value=data.order)
+                ws.cell(row=row, column=column+1, value=data.name)
+                ws.cell(row=row, column=column+2, value=data.jiaogao).number_format = 'yyyy/mm/dd'
+                ws.cell(row=row, column=column+3, value=data.taoxi)
+                ws.cell(row=row, column=column+4, value=data.jiaxuan)
+                ws.cell(row=row, column=column+5, value=data.owner.name)
+                ws.cell(row=row, column=column+6, value=data.chujian_date)
+                ws.cell(row=row, column=column+7, value=data.note)
+                ws.cell(row=row, column=column+8, value=data.zhangshu)
+                ws.cell(row=row, column=column+9, value=data.department)
+        output = BytesIO()
+        wb.save(output)
+        # 初始化指针
+        output.seek(0)
+        response.write(output.getvalue())
+        return response
+
+    export_excel.short_description = '导出选中数据至Excel'
